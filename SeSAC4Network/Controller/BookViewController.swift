3e7628c766dc9dea29246c0f1e41c5729f4d61e1
crisine <crisine@knou.ac.kr>
2024-01-17 11:03:40 +0900

    docs: gitignore created

---
+++ SeSAC4Network/Controller/BookViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BookViewController.swift
+//  SeSAC4Network
+//
+//  Created by Minho on 1/17/24.
+//
+
+import UIKit
+import Alamofire
+
+class BookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+}
--- SeSAC4Network/ViewController.swift
+++ SeSAC4Network/ViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Alamofire
 
 struct Jack {
     let boxOfficeResult: SmallJack
@@ -24,11 +25,30 @@ struct MovieJack {
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var sourceTextView: UITextView!
+    @IBOutlet weak var translateButton: UIButton!
+    @IBOutlet weak var targetLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        translateButton.addTarget(self, action: #selector(didtranslateButtonTapped), for: .touchUpInside)
     }
 
-
+    @objc func didtranslateButtonTapped() {
+        
+        let url = "https://openapi.naver.com/v1/papago/n2mt"
+        
+        let headers: HTTPHeaders = ["X-Naver-Client-Id": "VINXRj4R2REWWkj_IoME",
+                       "X-Naver-Client-Secret": "cwFzIOHCMN"]
+        
+        let parameters: Parameters = ["text": sourceTextView.text!,
+                          "source": "ko",
+                          "target": "en"]
+        
+        AF.request(url, method: .post, parameters: parameters, headers: headers)
+    }
 }
 
